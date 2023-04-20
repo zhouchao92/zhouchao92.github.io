cf08495922a1829b5b68d2006eed19f3fccdb0ba
zhouchao <1178605721@qq.com>
2023-04-20 10:13:57 +0800

    移除导航栏-笔记

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -2,7 +2,7 @@
 <div>
   <el-menu :default-active="activeIndex" mode="horizontal" active-text-color="#ffd04b" router>
     <el-menu-item index="/resume">简历</el-menu-item>
-    <el-menu-item index="/note">笔记</el-menu-item>
+    <!-- <el-menu-item index="/note">笔记</el-menu-item> -->
     <el-menu-item index="/recruitment">面试</el-menu-item>
   </el-menu>
 </div>
