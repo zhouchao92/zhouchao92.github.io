7c983a3bedcb2fd77bb0544f6405a1d108abab00
zhouchao <1178605721@qq.com>
2025-07-02 21:41:31 +0800

    修改markdown字体

--- src/components/markdownHtml.vue
+++ src/components/markdownHtml.vue
@@ -37,6 +37,7 @@ module.exports = {
   background-color: #fff;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
   /* border-radius: 4px; */
+  font-family: '宋体', 'SimSun', '微软雅黑', 'Arial', sans-serif;
 }
 
 .markdown-body h1,
@@ -45,7 +46,6 @@ module.exports = {
 .markdown-body h4,
 .markdown-body h5,
 .markdown-body h6 {
-  font-family: "Arial", sans-serif;
   color: #333;
   margin-top: 0.5em;
   margin-bottom: 0.75em;
