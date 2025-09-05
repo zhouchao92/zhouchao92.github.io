39b74fa00772232b00ee86cf9e87bdedd7bafce2
zhouchao <zcjava@outlook.com>
2025-09-05 11:35:40 +0800

    新增数据库底层原理比对

--- src/constant/filenames.js
+++ src/constant/filenames.js
@@ -1,4 +1,4 @@
-const notes = ['微服务端到端自监控', '数据库去O化', 'Kettle程序的应用'];
-const projects = ['告警子中心系统', '云积分会员通系统', '康博嘉云诊所SAAS系统'];
+const notes = ['Kettle程序的应用', '微服务端到端自监控', '数据库去O化', '数据库底层原理对比'];
+const projects = ['云积分会员通系统', '告警子中心系统', '康博嘉云诊所SAAS系统'];
 
 export { notes, projects };
