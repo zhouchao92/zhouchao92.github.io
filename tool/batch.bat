8f38134e72a58f74028d4be19ccebdadcee4dabe
zhouchao <1178605721@qq.com>
2026-08-02 10:16:13 +0800

    add 脚本工具

---
+++ tool/batch.bat
@@ -0,0 +1,41 @@
+# Element UI 图标字体
+curl -o public/css/fonts/element-icons.woff https://unpkg.com/element-ui@2.15.14/lib/theme-chalk/fonts/element-icons.woff
+curl -o public/css/fonts/element-icons.ttf https://unpkg.com/element-ui@2.15.14/lib/theme-chalk/fonts/element-icons.ttf
+
+# Vue.js
+curl -o public/js/vue.js https://unpkg.com/vue@2.7.16/dist/vue.js
+
+# Element UI CSS
+curl -o public/css/element-ui.css https://unpkg.com/element-ui@2.15.14/lib/theme-chalk/index.css
+
+# Element UI JS
+curl -o public/js/element-ui.js https://unpkg.com/element-ui@2.15.14/lib/index.js
+
+# jQuery
+curl -o public/js/jquery.js https://unpkg.com/jquery@3.6.4/dist/jquery.js
+
+# Vue Router
+curl -o public/js/vue-router.js https://unpkg.com/vue-router@3.6.5/dist/vue-router.js
+
+# http-vue-loader
+curl -o public/js/httpVueLoader.js https://unpkg.com/http-vue-loader@1.4.2/src/httpVueLoader.js
+
+# vue-meta
+curl -o public/js/vue-meta.js https://unpkg.com/vue-meta@2.4.0/dist/vue-meta.js
+
+# marked
+curl -o public/js/marked.js https://unpkg.com/marked@4.3.0/marked.min.js
+
+# highlight.js CSS
+curl -o public/css/github.min.css https://unpkg.com/@highlightjs/cdn-assets@11.11.1/styles/github.min.css
+
+# highlight.js JS
+curl -o public/js/highlight.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/highlight.min.js
+
+# highlight.js language packs
+curl -o public/js/go.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/go.min.js
+curl -o public/js/java.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/java.min.js
+curl -o public/js/sql.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/sql.min.js
+curl -o public/js/python.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/python.min.js
+curl -o public/js/shell.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/shell.min.js
+curl -o public/js/bash.min.js https://unpkg.com/@highlightjs/cdn-assets@11.11.1/languages/bash.min.js
